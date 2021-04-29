53b63ccd87b49283cfc85f4884f9ae88dab1adc2
Valdir Cezar <valdircezar312@gmail.com>
2021-04-29 10:22:15 -0300

    feat: addItem method create

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -7,25 +7,53 @@ class Form extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      tarefa: '',
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(event) {
+    let state = this.state;
+   
+    if(this.state.tarefa !== '') {
+      let newItem = {
+        text: this.state.tarefa,
+        key: Date.now()
+      }
+      state.items.push(newItem)
+      this.setState(state)
+    }
+    event.preventDefault();
+    console.log(this.state.items)
   }
 
   render() {
     return (
       <div className="container">
 
-        <TextField
-          id="standard-full-width"
-          label="Todo"
-          placeholder="Criar novo Todo"
-          helperText="Full width!"
-          fullWidth
-          margin="normal"
-          InputLabelProps={{
-            shrink: true,
-          }}
-        />
-        <Button variant="outlined" color="primary"> Adicionar </Button>
+        <h1>Lista de tarefas</h1>
+        <form>
+          <TextField
+            value={this.state.tarefa}
+            name="tarefa"
+            onChange={(event) => this.setState({ tarefa: event.target.value })}
+
+            // Material-ui
+            id="standard-full-width"
+            label="Todo"
+            placeholder="Criar novo Todo"
+            helperText="Full width!"
+            fullWidth
+            margin="normal"
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+        </form>
+        <Button onClick={this.addItem} variant="outlined" color="primary"> Adicionar </Button>
       </div>
     );
   }
